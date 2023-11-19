53ed526ab87f1eba643c7e1d7ff8cbfdc43e6a1d
Mark-André Hopf <mhopf@mark13.org>
2023-11-19 15:42:23 +0100

    load face groups

--- src/animorph/Mesh.ts
+++ src/animorph/Mesh.ts
@@ -3,19 +3,32 @@ import { VertexVector } from "./VertexVector"
 import { DirectoryList, FileType } from "./DirectoryList"
 import { TargetEntry } from "./TargetEntry"
 import { PoseEntry } from "./PoseEntry"
+import { FileSystemAdapter } from "../filesystem/FileSystemAdapter"
+import { StringToLine } from "../lib/StringToLine"
 
 class TargetMap extends Map<string, TargetEntry> {}
 class PoseMap extends Map<string, PoseEntry> {}
 
 // animorph-0.3/src/Mesh.cpp
 export class Mesh {
+    // faces
     facevector = new FaceVector()
-    vertexvector_morph = new VertexVector()
+
+    // vertices
+    vertexvector_morph = new VertexVector() // morphed mesh
+    // vertexvector_morph_copy: VertexVector // copy of vertexvector_morph to reset vertexvector_morph and vertexvector_morph_only, used when animating/posing)
+    // vertexvector_morph_only: VertexVector
+    // vertexvector_orginal: vec3[]; // orginal mesh
+    facegroup = new FaceGroup()
+
     targetmap = new TargetMap()
     posemap = new PoseMap()
     loadMeshFactory(meshFilename: string, facesFilename: string) {
         this.vertexvector_morph.load(meshFilename)
         this.facevector.loadGeometry(facesFilename)
+    }
+    loadGroupsFactory(groups_filename: string) {
+        return this.facegroup.load(groups_filename)
     }
     loadTargetsFactory(targetRootPath: string, recursiveLevel = 1, preload = false, clearmap = true) {
         if (clearmap) {
@@ -57,7 +70,50 @@ export class Mesh {
         }
         console.log(`loaded ${counter} pose entries from ${targetRootPath}/`)
     }
-    loadCharactersFactory(charactersRootPath: string, recursiveLevel = 1) {
-        throw Error("not implemented yet")
+    // loadCharactersFactory(charactersRootPath: string, recursiveLevel = 1) {
+    //     throw Error("not implemented yet")
+    // }
+}
+
+class FGroupData extends Array<number> {}
+class VertexData extends Map<number, number> {}
+
+class FGroup {
+    visible: boolean
+    /// A vector of ints
+    facesIndexes: FGroupData
+    facesIndexes_subd?: FGroupData ///subdivision
+}
+
+function isAlpha(ch: string){
+    return ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z"
+}
+
+class FaceGroup extends Map<string, FGroup> {
+    private loaded: boolean
+    // Maps FaceGroup identifiers via vertex group numbers to vertex indices
+    private vertexes: Map<string, VertexData>
+    load(filename: string): boolean {
+        let fgroup_indent: string
+        const content = FileSystemAdapter.readFile(`data/${filename}`)
+        const reader = new StringToLine(content)
+        let lineNumber = 0
+        for (let line of reader) {
+            if (isAlpha(line.charAt(0))) {
+                fgroup_indent = line.split(",")[0].trim()
+            } else {
+                let data: FGroup = {
+                    visible: true,
+                    facesIndexes: line.split(" ").map(it => parseInt(it))
+                }
+                this.set(fgroup_indent!, data)
+            }
+        }
+
+        console.log(`loaded ${this.size} face groups from ${filename}`)
+
+        // there's some code for 'visibilities' but there ain't no ones in base.parts
+
+        return true
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { Mesh } from "./animorph/Mesh"
 import { FileSystemAdapter } from "./filesystem/FileSystemAdapter"
 import { NodeJSFSAdapter } from "./filesystem/NodeJSFSAdapter"
 
-// makehuman-0.9.1-rc1a/src/makehuman.cpp
+// makehuman-0.9.1-rc1a/src/makehuman.cpp, line 499
 function main() {
     console.log(`Makehuman 0.9.1-rc1a (JS Port)`)
     FileSystemAdapter.setInstance(new NodeJSFSAdapter())
@@ -11,7 +11,7 @@ function main() {
     // mesh.loadMaterialFactory("base.materials", "base.colors")
     // TextureVector texturevector = mesh.getTextureVectorRef()
     // texturevector.load("base.uv")
-    // mesh.loadGroupsFactory("base.parts")
+    mesh.loadGroupsFactory("base.parts")
     // mesh.loadSubdGroupsFactory("subd.parts")
     // mesh.loadSkinFactory("base.skin")
     // mesh.loadClothesFactory("base.clothes")
@@ -23,6 +23,13 @@ function main() {
     mesh.loadTargetsFactory("selectors", 1, true, false);
     mesh.loadPoseTargetsFactory("rotations")
     // mesh.loadCharactersFactory("bs_data")
+
+    // okay... this should be enough to render something...
+}
+
+// makehuman-0.9.1-rc1a/src/makehuman.cpp, line 923
+function renderMesh() {
+    // get's FaceVector, VertexVector & FaceGroup
 }
 
 main()
