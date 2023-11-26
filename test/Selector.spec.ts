74541188ac7e9781c67f83121c85e29fa4ac0a34
Mark-André Hopf <mhopf@mark13.org>
2023-11-26 18:46:27 +0100

    refactor

--- test/Selector.spec.ts
+++ test/Selector.spec.ts
@@ -2,8 +2,8 @@ import { use, expect } from "@esm-bundle/chai"
 import { chaiAlmost } from "./chai/chaiAlmost"
 use(chaiAlmost())
 
-describe("Selector", () => {
-    it("1", () => {
+describe("SelectorListener", () => {
+    it("calcWidgetTargets()", () => {
         const sl = new SelectorListener()
         sl.calcWidgetTargets()
     })
@@ -15,7 +15,43 @@ interface Point {
 }
 
 class SelectorListener {
-    constructor() {}
+    age: Selector
+    muscleSize: Selector
+    breastSize: Selector
+    shapeSize: Selector
+
+    ageDists: number[]
+    muscleSizeDists: number[]
+    breastDists: number[]
+    shapeDists: number[]
+
+    constructor() {
+        // prettier-ignore
+        this.age = new Selector(2, 5, [
+            "female_10", "female_30", "female_50", "female_70", "female_90", 
+            "male_10", "male_30", "male_50", "male_70", "male_90"
+        ])
+        // prettier-ignore
+        this.muscleSize = new Selector(2, 2, [
+            "skinny_nomuscle", "big_nomuscle",
+            "skinny_muscle", "big_muscle"
+        ])
+        // prettier-ignore
+        this.breastSize = new Selector(2, 2, [
+            "cone_little", "cone_big",
+            "sphere_little", "sphere_big"
+        ])
+        // prettier-ignore
+        this.shapeSize = new Selector(2, 2, [
+            "brevilinear_vshape", "brevilinear_peershape",
+            "longilinear_vshape", "longilinear_peershape"
+        ])
+
+        this.ageDists = this.age.getDists()
+        this.muscleSizeDists = this.muscleSize.getDists()
+        this.breastDists = this.breastSize.getDists()
+        this.shapeDists = this.shapeSize.getDists()
+    }
 
     doMorph(name: string, value: number) {
         if (value === 0) {
@@ -25,56 +61,30 @@ class SelectorListener {
     }
 
     calcWidgetTargets() {
-        // prettier-ignore
-        const age = new Selector(2, 5, [
-            "female_10", "female_30", "female_50", "female_70", "female_90", 
-            "male_10", "male_30", "male_50", "male_70", "male_90"
-        ])
-        // prettier-ignore
-        const muscleSize = new Selector(2, 2, [
-            "skinny_nomuscle", "big_nomuscle",
-            "skinny_muscle", "big_muscle"
-        ])
-        // prettier-ignore
-        const breastSize = new Selector(2, 2, [
-            "cone_little", "cone_big",
-            "sphere_little", "sphere_big"
-        ])
-        // prettier-ignore
-        const shapeSize = new Selector(2, 2, [
-            "brevilinear_vshape", "brevilinear_peershape",
-            "longilinear_vshape", "longilinear_peershape"
-        ])
-
-        const ageDists = age.getDists()
-        const muscleSizeDists = muscleSize.getDists()
-        const breastDists = breastSize.getDists()
-        const shapeDists = shapeSize.getDists()
-
         let i: number, j: number, k: number
 
         i = 0
-        for (const di_it of ageDists) {
-            if (i < age.labels.length) {
-                const tmpTargetName = `ages/${age.labels[i++]}.target`
+        for (const di_it of this.ageDists) {
+            if (i < this.age.labels.length) {
+                const tmpTargetName = `ages/${this.age.labels[i++]}.target`
                 this.doMorph(tmpTargetName, di_it)
             }
         }
 
         j = 0
-        for (const ms_it of muscleSizeDists) {
+        for (const ms_it of this.muscleSizeDists) {
             i = 0
-            for (const di_it of ageDists) {
-                if (j < muscleSize.labels.length && i < age.labels.length) {
-                    const tmpTargetName = `muscleSize/${age.labels[i]}_${muscleSize.labels[j]}.target`
+            for (const di_it of this.ageDists) {
+                if (j < this.muscleSize.labels.length && i < this.age.labels.length) {
+                    const tmpTargetName = `muscleSize/${this.age.labels[i]}_${this.muscleSize.labels[j]}.target`
                     const tmpTargetValue = di_it * ms_it
                     this.doMorph(tmpTargetName, tmpTargetValue)
                 }
                 k = 0
                 if (i <= 4) {
-                    for (const br_it of breastDists) {
-                        if (k < breastSize.labels.length) {
-                            const tmpTargetName = `breast/${age.labels[i]}_${muscleSize.labels[j]}_${breastSize.labels[k]}.target`
+                    for (const br_it of this.breastDists) {
+                        if (k < this.breastSize.labels.length) {
+                            const tmpTargetName = `breast/${this.age.labels[i]}_${this.muscleSize.labels[j]}_${this.breastSize.labels[k]}.target`
                             const tmpTargetValue = di_it * ms_it * br_it
                             this.doMorph(tmpTargetName, tmpTargetValue)
                         }
@@ -87,9 +97,9 @@ class SelectorListener {
         }
 
         i = 0
-        for (const sh_it of shapeDists) {
-            if (i < shapeSize.labels.length) {
-                const tmpTargetName = `shapes/${shapeSize.labels[i++]}.target`
+        for (const sh_it of this.shapeDists) {
+            if (i < this.shapeSize.labels.length) {
+                const tmpTargetName = `shapes/${this.shapeSize.labels[i++]}.target`
                 this.doMorph(tmpTargetName, sh_it)
             }
         }
@@ -125,6 +135,10 @@ class Selector {
                 })
             }
         }
+    }
+    setCursorPos(x: number, y: number) {
+        this.cursorPos.x = x
+        this.cursorPos.y = y
     }
     getDists() {
         const ret: number[] = []
